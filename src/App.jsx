673f950f3ae4a3db606d6a7119b4d88b1be64248
larcondo <larcondo90@gmail.com>
2023-09-04 14:46:49 -0300

    modify Home and Users components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </div>
 )
 
@@ -18,6 +19,11 @@ const Notes = () => (
 const Users = () => (
   <div>
     <h2>Users</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
 )
 
